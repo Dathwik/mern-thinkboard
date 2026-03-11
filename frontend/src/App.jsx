9d3606700f9b425db97f7419042b2a5d4fa903aa
DathwikK <satya.dathwik@gmail.com>
2026-03-11 08:24:56 -0400

    Added tailwind css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,10 +3,12 @@ import { Route, Routes } from 'react-router'
 import CreatePage from './pages/CreatePage'
 import HomePage from './pages/HomePage'
 import NoteDetailPage from './pages/NoteDetailPage'
+import toast from "react-hot-toast"
 
 
 const App = () => {
   return <div>
+    <button onClick={() => toast.error("Congrats")}>Click Me</button>
     <Routes>
       <Route path="/" element={<HomePage/>} />
       <Route path="/create" element={<CreatePage />} />
