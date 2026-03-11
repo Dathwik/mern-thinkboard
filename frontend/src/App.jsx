8cf9386e307235d8ed3c7e6ef987aea0de680851
DathwikK <satya.dathwik@gmail.com>
2026-03-11 08:10:54 -0400

    Added page routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Route, Routes } from 'react-router'
+import CreatePage from './pages/CreatePage'
+import HomePage from './pages/HomePage'
+import NoteDetailPage from './pages/NoteDetailPage'
+
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
+  return <div>
+    <Routes>
+      <Route path="/" element={<HomePage/>} />
+      <Route path="/create" element={<CreatePage />} />
+      <Route path="/note/:id" element={<NoteDetailPage />} />
+    </Routes>
+  </div>
 }
+
 
 export default App
