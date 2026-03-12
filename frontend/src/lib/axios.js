943d3da9bef22c08e7953dd5601905a6173bdc27
DathwikK <satya.dathwik@gmail.com>
2026-03-12 12:07:56 -0400

    changed 'baseURL:/api' to 'baseURL: BASE_URL'

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from "axios"
 // Production vs Development (Made BASE_URL dynamic)
 const BASE_URL = import.meta.env.mode === "development" ? "http://localhost:5001/api" : "/api"
 const api = axios.create({
-    baseURL: "http://localhost:5001/api"
+    baseURL: BASE_URL,
 });
 
 export default  api
